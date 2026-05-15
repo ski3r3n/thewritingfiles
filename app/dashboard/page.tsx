5e06724df83d82931c9d725fad7fb828d44f2224
ski3r3n <kieren.siew@gmail.com>
2026-05-15 21:50:51 +0800

    init commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+"use client";
+export default function Dashboard() {
+  return <>placeholder for dashboard content </>;
+}
---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+export default function Home() {
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen py-2">
+        <div className="flex flex-col bg-mist-800 p-6 rounded">
+          <h1 className=" text-3xl mb-0.5 font-bold w-max">The Writing Files</h1>
+          <h2 className="text-xl text-gray-600">
+            Study gc lol, who let him cook
+          </h2>
+          <a href="/login" className="self-center h-min">
+            <button className="mt-4 bg-mist-900 hover:bg-slate-950 active:bg-mist-900 transition cursor-pointer text-white font-bold py-2 px-4 rounded">
+              Login
+            </button>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
