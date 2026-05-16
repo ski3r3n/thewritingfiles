ca307ca0ebddcde2f1b6f4ab473edf5d755f1453
ski3r3n <kieren.siew@gmail.com>
2026-05-16 23:45:48 +0800

    writing link add placeholder redirect

--- app/dashboard/write/page.tsx
+++ app/dashboard/write/page.tsx
@@ -9,21 +9,25 @@ export default function Write() {
         <div className="flex-1 p-10 overflow-y-auto h-screen">
           <h1 className="text-3xl font-bold mb-4">Write</h1>
           <p>Go make some peak. I&apos;m waiting.</p>
-          <form>
-            <div className="mt-6">
-              <input
-                type="text"
-                className="text-2xl mb-4 p-4 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200"
-                placeholder="Title"
-              />
-              <textarea
-                className="w-full h-64 p-4 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200"
-                placeholder="Some absolute peak?"
-              />
-              <button className="mt-4 bg-mist-800 hover:bg-mist-950 active:bg-mist-800 transition cursor-pointer text-white font-bold py-2 px-4 rounded">
-                Save
-              </button>
-            </div>
+          <form
+            className="mt-6"
+            onSubmit={(e) => {
+              e.preventDefault();
+              window.location.href = "/dashboard/1"; // placeholder
+            }}
+          >
+            <input
+              type="text"
+              className="text-2xl mb-4 p-4 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200"
+              placeholder="Title"
+            />
+            <textarea
+              className="w-full h-64 p-4 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200"
+              placeholder="Some absolute peak?"
+            />
+            <button className="mt-4 bg-mist-800 hover:bg-mist-950 active:bg-mist-800 transition cursor-pointer text-white font-bold py-2 px-4 rounded">
+              Save
+            </button>
           </form>
         </div>
       </div>
